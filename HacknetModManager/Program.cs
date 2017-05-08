5c6b41eccbbdee2e0c30ad403888d4601ff176c4
Alana Gilston <alana@abluescarab.us>
2017-05-08 04:34:12 -0700

    Fixed call to frmMain

--- HacknetModManager/Program.cs
+++ HacknetModManager/Program.cs
@@ -13,7 +13,7 @@ namespace HacknetModManager {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new frmMain());
         }
     }
 }
