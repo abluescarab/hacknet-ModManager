7966ceaa8ebbef5fc3c05673c48d93c9ee216bef
Alana Gilston <alana@abluescarab.us>
2017-05-08 02:02:40 -0700

    Added move/copy helper functions

--- HacknetModManager/FileUtils.cs
+++ HacknetModManager/FileUtils.cs
@@ -27,6 +27,54 @@ namespace HacknetModManager {
             process.Start();
         }
 
+        // --------------------------------------------------------------------
+        // Following source modified from http://stackoverflow.com/a/12292399/567983
+
+        public static void FileCopyWithReplicate(string source, string dest, bool overwrite, bool deleteSource = false,
+            bool deleteContents = false) {
+            string destDirectory = Path.GetDirectoryName(dest);
+            Directory.CreateDirectory(destDirectory);
+
+            if(Directory.Exists(source)) {
+                DirectoryCopy(source, dest, overwrite, deleteContents);
+                if(deleteSource) Directory.Delete(source);
+            }
+            else if(File.Exists(source)) {
+                File.Copy(source, dest, overwrite);
+                if(deleteSource) File.Delete(source);
+            }
+        }
+
+        private static void DirectoryCopy(string source, string dest, bool recursive, bool delete) {
+            DirectoryInfo dir = new DirectoryInfo(source);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if(!dir.Exists) {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + source);
+            }
+
+            if(!Directory.Exists(dest)) {
+                Directory.CreateDirectory(dest);
+            }
+
+            FileInfo[] files = dir.GetFiles();
+            foreach(FileInfo file in files) {
+                string temppath = Path.Combine(dest, file.Name);
+                file.CopyTo(temppath, true);
+
+                if(delete) file.Delete();
+            }
+
+            if(recursive) {
+                foreach(DirectoryInfo subdir in dirs) {
+                    string temppath = Path.Combine(dest, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, recursive, delete);
+                }
+            }
+        }
+
         //
         //        public static void CreateFile(string path, string data) {
         //            var file = CreateFileAsync(path, data);
