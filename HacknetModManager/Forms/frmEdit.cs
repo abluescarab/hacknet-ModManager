aaa4213977335716a1b72b0cb395d870f2ae85da
Alana Gilston <alana@abluescarab.us>
2017-05-09 08:38:57 -0700

    Added frmEdit

---
+++ HacknetModManager/Forms/frmEdit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HacknetModManager {
+    public partial class frmEdit : Form {
+        private Mod mod;
+
+        public frmEdit() {
+            InitializeComponent();
+        }
+
+        public DialogResult ShowDialog(Mod mod) {
+            this.mod = mod;
+
+            txtTitle.Text = mod.Title;
+            txtDescription.Text = mod.Description;
+            txtVersion.Text = mod.Version;
+            txtHomepage.Text = mod.Homepage;
+            txtRepository.Text = mod.Repository;
+            txtInfo.Text = mod.Info;
+
+            foreach(string author in mod.Authors) {
+                lbxAuthors.Items.Add(author);
+            }
+
+            lbxAuthors.SelectedIndex = 0;
+
+            return ShowDialog();
+        }
+
+        private void btnMoveUp_Click(object sender, EventArgs e) {
+
+        }
+
+        private void btnMoveDown_Click(object sender, EventArgs e) {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) {
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e) {
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e) {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
--- HacknetModManager/Properties/Resources.Designer.cs
+++ HacknetModManager/Properties/Resources.Designer.cs
@@ -63,9 +63,49 @@ namespace HacknetModManager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap add {
+            get {
+                object obj = ResourceManager.GetObject("add", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap arrow_down {
+            get {
+                object obj = ResourceManager.GetObject("arrow_down", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap arrow_refresh {
             get {
                 object obj = ResourceManager.GetObject("arrow_refresh", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap arrow_up {
+            get {
+                object obj = ResourceManager.GetObject("arrow_up", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap delete {
+            get {
+                object obj = ResourceManager.GetObject("delete", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
