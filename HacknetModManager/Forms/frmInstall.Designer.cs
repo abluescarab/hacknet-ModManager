f9089e1ee737798272f787d1ada08e0b83aa515a
Alana Gilston <alana@abluescarab.us>
2017-05-09 03:50:22 -0700

    Implemented frmInstall

--- HacknetModManager/Forms/frmInstall.Designer.cs
+++ HacknetModManager/Forms/frmInstall.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace HacknetModManager.Forms {
+﻿namespace HacknetModManager {
     partial class frmInstall {
         /// <summary>
         /// Required designer variable.
@@ -23,11 +23,126 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMod = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tlpButtons = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.dlgOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.tlpButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a GitHub repository or browse for a file:";
+            // 
+            // txtMod
+            // 
+            this.txtMod.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMod.Location = new System.Drawing.Point(12, 26);
+            this.txtMod.Name = "txtMod";
+            this.txtMod.Size = new System.Drawing.Size(200, 20);
+            this.txtMod.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(218, 25);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // tlpButtons
+            // 
+            this.tlpButtons.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpButtons.ColumnCount = 2;
+            this.tlpButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpButtons.Controls.Add(this.btnAdd, 0, 0);
+            this.tlpButtons.Controls.Add(this.btnClose, 1, 0);
+            this.tlpButtons.Location = new System.Drawing.Point(133, 59);
+            this.tlpButtons.Name = "tlpButtons";
+            this.tlpButtons.RowCount = 1;
+            this.tlpButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpButtons.Size = new System.Drawing.Size(160, 25);
+            this.tlpButtons.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdd.Location = new System.Drawing.Point(0, 0);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(77, 25);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClose.Location = new System.Drawing.Point(83, 0);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(77, 25);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // dlgOpenFile
+            // 
+            this.dlgOpenFile.DefaultExt = "zip";
+            this.dlgOpenFile.Filter = "Zip files|*.zip";
+            this.dlgOpenFile.RestoreDirectory = true;
+            this.dlgOpenFile.SupportMultiDottedExtensions = true;
+            this.dlgOpenFile.Title = "Choose a mod...";
+            // 
+            // frmInstall
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "frmInstall";
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(305, 96);
+            this.Controls.Add(this.tlpButtons);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtMod);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmInstall";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Install Mod";
+            this.tlpButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMod;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TableLayoutPanel tlpButtons;
+        internal System.Windows.Forms.Button btnAdd;
+        internal System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.OpenFileDialog dlgOpenFile;
     }
 }
