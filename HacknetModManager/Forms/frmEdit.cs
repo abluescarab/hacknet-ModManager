e8bcb3d7d2a410ee836c5ddd9eae35ebb9d9a26a
Alana Gilston <alana@abluescarab.us>
2017-05-09 10:01:56 -0700

    Implemented frmEdit

--- HacknetModManager/Forms/frmEdit.cs
+++ HacknetModManager/Forms/frmEdit.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 namespace HacknetModManager {
     public partial class frmEdit : Form {
         private Mod mod;
+        private const string noAuthors = "No authors.";
 
         public frmEdit() {
             InitializeComponent();
@@ -30,7 +31,12 @@ namespace HacknetModManager {
                 lbxAuthors.Items.Add(author);
             }
 
-            lbxAuthors.SelectedIndex = 0;
+            if(lbxAuthors.Items.Count > 0)
+                lbxAuthors.SelectedIndex = 0;
+            else {
+                lbxAuthors.Enabled = false;
+                lbxAuthors.Items.Add(noAuthors);
+            }
 
             return ShowDialog();
         }
@@ -44,19 +50,46 @@ namespace HacknetModManager {
         }
 
         private void btnAdd_Click(object sender, EventArgs e) {
+            frmInput input = new frmInput();
+            if(input.ShowDialog("Add Author", "Enter an author's name:") == DialogResult.OK) {
+                if(lbxAuthors.Items.Contains(noAuthors)) {
+                    lbxAuthors.Items.Clear();
+                }
 
+                lbxAuthors.Items.Add(input.Answer);
+                lbxAuthors.Enabled = true;
+            }
         }
 
         private void btnRemove_Click(object sender, EventArgs e) {
+            for(int i = lbxAuthors.Items.Count - 1; i >= 0; i--) {
+                if(lbxAuthors.SelectedItems.Contains(lbxAuthors.Items[i])) {
+                    lbxAuthors.Items.RemoveAt(i);
+                }
+            }
 
+            if(lbxAuthors.Items.Count == 0) {
+                lbxAuthors.Enabled = false;
+                lbxAuthors.Items.Add(noAuthors);
+            }
         }
 
         private void btnOK_Click(object sender, EventArgs e) {
+            mod.Title = txtTitle.Text;
+            mod.Description = txtDescription.Text;
+            mod.Version = txtVersion.Text;
+            mod.Homepage = txtHomepage.Text;
+            mod.Repository = txtRepository.Text;
+            mod.Authors = lbxAuthors.Items.Cast<string>().ToArray();
+            mod.Info = txtInfo.Text;
 
+            DialogResult = DialogResult.OK;
+            Close();
         }
 
         private void btnCancel_Click(object sender, EventArgs e) {
-
+            DialogResult = DialogResult.Cancel;
+            Close();
         }
     }
 }
