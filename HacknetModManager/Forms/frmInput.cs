67c177619ca72650f214ee4cd3018b9941ffda85
Alana Gilston <alana@abluescarab.us>
2017-05-09 08:53:00 -0700

    Added frmInput

---
+++ HacknetModManager/Forms/frmInput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HacknetModManager {
+    public partial class frmInput : Form {
+        public string Input { get; private set; }
+
+        public frmInput() {
+            InitializeComponent();
+        }
+
+        public DialogResult ShowDialog(string title, string text) {
+            Text = title;
+            txtInput.Text = text;
+            return ShowDialog();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e) {
+            Input = txtInput.Text;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            Input = "";
+            Close();
+        }
+    }
+}
