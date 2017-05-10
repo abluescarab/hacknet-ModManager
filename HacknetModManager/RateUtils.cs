311bf156046020894868b7ec85dddc51ad65ebbd
Alana Gilston <alana@abluescarab.us>
2017-05-10 12:30:46 -0700

    Added RateUtils for GitHub rate limits

---
+++ HacknetModManager/RateUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using Octokit;
+
+namespace HacknetModManager {
+    public static class RateUtils {
+        public static RateLimit GetRateLimit(GitHubClient client) {
+            return client.Miscellaneous.GetRateLimits().Result.Rate;
+        }
+
+        public static string GetFormattedResetTime(GitHubClient client, bool twentyFourHour) {
+            return GetFormattedResetTime(GetRateLimit(client), twentyFourHour);
+        }
+
+        public static string GetFormattedResetTime(RateLimit limit, bool twentyFourHour) {
+            if(twentyFourHour)
+                return limit.Reset.DateTime.ToString("H:mm:ss");
+            else
+                return limit.Reset.DateTime.ToString("h:mm:ss tt");
+        }
+
+        public static void ShowRateErrorMessage(GitHubClient client, bool twentyFourHour) {
+            MessageBox.Show("You have reached your rate limit. You can make requests again at "
+                + GetFormattedResetTime(client, twentyFourHour) + ".", "Rate Limit Exceeded");
+        }
+    }
+}
