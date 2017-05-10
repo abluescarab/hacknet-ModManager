e8aaae72226a77e10724cbe3dec673b28d393b50
Alana Gilston <alana@abluescarab.us>
2017-05-10 14:42:58 -0700

    Added menu strip renderer

---
+++ HacknetModManager/Controls/DarkMenuStripRenderer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HacknetModManager.Controls {
+    public class DarkMenuStripRenderer : ToolStripProfessionalRenderer {
+        public DarkMenuStripRenderer() : base(new DarkTheme()) { }
+    }
+}
