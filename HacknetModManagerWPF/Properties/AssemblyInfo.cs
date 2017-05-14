aa9709ad920b1caeb25854baf54a9418df46216b
Alana Gilston <alana@abluescarab.us>
2017-05-13 19:22:55 -0700

    Updated to v1.0.0-alpha

--- HacknetModManagerWPF/Properties/AssemblyInfo.cs
+++ HacknetModManagerWPF/Properties/AssemblyInfo.cs
@@ -51,5 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("1.0.0")]
+#pragma warning disable 7035
+[assembly: AssemblyFileVersion("1.0.0-alpha")]
+#pragma warning restore 7035
